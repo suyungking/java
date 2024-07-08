c455276e90f191e9461aa5745a3aacd47a33c454
suyungking <105546111+suyungking@users.noreply.github.com>
2024-07-08 22:37:02 +0900

    java

---
+++ src/scope/scope3_2.java
@@ -0,0 +1,15 @@
+package scope;
+
+public class scope3_2 {
+    public static void main(String[] args){
+        int m = 10;
+        if( m > 0){
+
+            int temp = m*2;  // if문 안에서 사용하는 지역변수구나 라는것을 알수있다/
+            System.out.println("temp = "+ temp);
+        }
+        System.out.println("m= " + m);
+
+    }
+    }
+
