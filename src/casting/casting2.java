758e70076f3bf086612ca61ae7d9873e9c2ab20c
suyungking <105546111+suyungking@users.noreply.github.com>
2024-07-10 20:12:14 +0900

    java

--- src/casting/casting2.java
+++ src/casting/casting2.java
@@ -6,6 +6,7 @@ public class casting2 {
         int intValue = 0 ;
 
         // intValue = doubleValue  컴파일 오류 발생
+        // int 형은 double 형보다 표현 범위가 작다.
         intValue = (int) doubleValue ;  // 형변환 (명시적)
         System.out.println(intValue); // 1
     }
