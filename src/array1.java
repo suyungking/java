25e5a7972b82777cd8340091bfa8f821877e81aa
suyungking <105546111+suyungking@users.noreply.github.com>
2024-11-28 18:35:34 +0900

    java

---
+++ src/array1.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class array1 {
+    public static void main(String[] args){
+        int[] array1 = {1,2,3,4,5}; // 값의 목록을 가지고 있을때
+        int[] array2 = new int[5]; // 값의 목록을 가지고 있지 않을때
+
+        array2[0] = 2;
+
+        System.out.println(array1[1]);
+        System.out.println(array2[0]);
+
+    }
+}
