c2a07010a9befb208b3cd3afe9bd78b18b82bcfd
suyungking <105546111+suyungking@users.noreply.github.com>
2025-01-13 12:51:39 +0900

    java

---
+++ src/scanner/Scanner3.java
@@ -0,0 +1,28 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자를 입력하세요 : ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요 : ");
+        int num2 = scanner.nextInt();
+
+        if(num1> num2 ){
+            System.out.println("더 큰 수는 첫번째 수인 "+num1);
+        }
+
+        else if (num2 > num1){
+            System.out.println("더 큰 수는 두번째 수인 : "+num2);
+        }
+        else {
+            System.out.println("두숫자는 같습니다.");
+        }
+
+        }
+    }
+
